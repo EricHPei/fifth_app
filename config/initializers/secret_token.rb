18bdec9b9ee4e537100300a925ceab16228bcc69
Eric Pei <erichpei@gmail.com>
2013-09-08 18:27:05 -0700

    layout

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ChaptfiveApp::Application.config.secret_key_base = '31cf0f267ce487ed7c3b15b144c5121e9c8bfa0cdd3f9071a90bfa27c956e84664a4eb208a62404906a8dc20534fde19005a6dc40045ae5678999e1be899ddf1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ChaptfiveApp::Application.config.session_store :cookie_store, key: '_chaptfive_app_session'
